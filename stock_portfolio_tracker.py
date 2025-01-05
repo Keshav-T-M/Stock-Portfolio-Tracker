14e679a47bc6a6eb03898204bb575879c0599945
Keshavshetty707 <mkeshav9241217275@gmail.com>
2025-01-05 09:23:20 +0530

    created a function the check the validity of the stock name

--- stock_portfolio_tracker.py
+++ stock_portfolio_tracker.py
@@ -6,3 +6,13 @@ import os
 def clear_screen():
     """Clears the console screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
+
+#THIS FUNTIOON WILL CHECK IF THE STOCK TICKER IS VALID OR NOT
+def get_stock_data(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        data = stock.info 
+        return data
+    except Exception as e:
+        print(f"Error fetching data for {ticker}: {e}")
+        return None
